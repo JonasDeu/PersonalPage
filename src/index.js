06bb201c8f09aadda39e599d9fded3daeca22e47
JonasDeu <jonasdeuchler@gmail.com>
2020-11-24 15:45:02 +0100

    Prettier Format

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
-const express = require("express")
+const express = require('express')
 
-const https = require('https');
-const fs = require('fs');
+const https = require('https')
+const fs = require('fs')
 const helmet = require('helmet')
 
-
-const cors = require("cors")
-const displayRouter = require("./routes/displayRouter.js")
+const cors = require('cors')
+const displayRouter = require('./routes/displayRouter.js')
 
 const app = express()
-const port =process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(cors())
 app.use(displayRouter)
-app.use(helmet());
-app.use(express.static('public'));
+app.use(helmet())
+app.use(express.static('public'))
 
 const options = {
-    cert: fs.readFileSync('./sslcert/fullchain.pem'),
-    key: fs.readFileSync('./sslcert/privkey.pem')
-};
+	cert: fs.readFileSync('./sslcert/fullchain.pem'),
+	key: fs.readFileSync('./sslcert/privkey.pem'),
+}
 
 app.listen(port, () => {
-    console.log("Server up on: " + port)
+	console.log('Server up on: ' + port)
 })
 
-https.createServer(options, app).listen(3001);
+https.createServer(options, app).listen(3001)
--- src/routes/displayRouter.js
+++ src/routes/displayRouter.js
@@ -1,29 +1,28 @@
-const express = require("express")
+const express = require('express')
 const router = new express.Router()
 
-const font = require('oled-font-5x7');
-const i2c = require('i2c-bus');
-const i2cBus = i2c.openSync(1);
-const oled = require('oled-i2c-bus');
+const font = require('oled-font-5x7')
+const i2c = require('i2c-bus')
+const i2cBus = i2c.openSync(1)
+const oled = require('oled-i2c-bus')
 const displayOpts = {
-  width: 128,
-  height: 32,
-  address: 0x3C
-};
-const display = new oled(i2cBus, displayOpts);
+	width: 128,
+	height: 32,
+	address: 0x3c,
+}
+const display = new oled(i2cBus, displayOpts)
 
-router.post("/display", async (req, res) => {
-    console.log(req.body)
-    try {
-	display.clearDisplay();
-	display.setCursor(1, 1);
-	display.writeString(font, 1, req.body.msg, 1, true);
+router.post('/display', async (req, res) => {
+	console.log(req.body)
+	try {
+		display.clearDisplay()
+		display.setCursor(1, 1)
+		display.writeString(font, 1, req.body.msg, 1, true)
 
-        res.status(201).send()
-    } catch (e) {
-        res.status(400).send(e)
-    }
-
+		res.status(201).send()
+	} catch (e) {
+		res.status(400).send(e)
+	}
 })
 
 module.exports = router
