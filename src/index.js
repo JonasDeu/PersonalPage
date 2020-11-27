3c8a4d4ab50ac7ee4f095364a07ef4c650045bd3
JonasDeu <jonasdeuchler@gmail.com>
2020-11-27 16:30:21 +0100

    Https Redirect

--- src/index.js
+++ src/index.js
@@ -14,6 +14,12 @@ app.use(express.json())
 app.use(cors())
 app.use(displayRouter)
 app.use(helmet())
+app.use(function (req, res, next) {
+	if (!req.secure) {
+		return res.redirect(['https://', req.get('Host'), req.baseUrl].join(''))
+	}
+	next()
+})
 app.use(express.static('public'))
 
 const options = {
